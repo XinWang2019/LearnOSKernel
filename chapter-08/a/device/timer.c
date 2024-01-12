06784b1b37d95681a8f322d0a99879847cf5c488
wangxin <wxcqyz@gmail.com>
2024-01-12 21:47:40 +0800

    完成8.2.2实现ASSERT;

--- chapter-08/a/device/timer.c
+++ chapter-08/a/device/timer.c
@@ -27,7 +27,7 @@ static void frequency_set(uint8_t counter_port, \
 }
 
 /* 初始化PIT8253 */
-void timer_init() {
+void timer_init(void) {
     put_str("timer_init start\n");
     /* 设置8253的定时周期, 也就是发中断的周期 */
     frequency_set(COUNTER0_PORT, COUNTER0_NO, READ_WRITE_LATCH, COUNTER_MODE, COUNTER0_VALUE);
