64791653eb91b4da39b387485066a1ad69ee1d73
wangxin <wxcqyz@gmail.com>
2024-12-18 21:32:16 +0800

    完成10.1.4线程的阻塞和唤醒代码；

--- chapter-10/a/thread/thread.h
+++ chapter-10/a/thread/thread.h
@@ -96,5 +96,7 @@ void thread_create(struct task_struct* pthread, thread_func function, void* func
 void init_thread(struct task_struct* pthread, char* name, int prio);
 struct task_struct* thread_start(char* name, int prio, thread_func function, void* func_arg);
 void schedule();
+void thread_block(enum task_status stat);
+void thread_unblocked(struct task_struct* pthread);
 void thread_init(void);
 #endif
